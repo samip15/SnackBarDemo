435dd59c63f8f3656c6089ef9d5c2e14568b0619
Samip Gnyawali <samipgnyawali15@gmail.com>
2021-09-20 21:15:57 +0545

    first commit

---
+++ app/src/main/java/com/example/snackbardemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.snackbardemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    CoordinatorLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        layout = findViewById(R.id.layout);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+
+                        // Create a snackbar
+                        Snackbar snackbar
+                                = Snackbar
+                                .make(
+                                        layout,
+                                        "Message is deleted",
+                                        Snackbar.LENGTH_LONG)
+                                .setAction(
+                                        "UNDO",
+
+                                        // If the Undo button
+                                        // is pressed, show
+                                        // the message using Toast
+                                        new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view)
+                                            {
+                                                Toast.makeText(
+                                                                MainActivity.this,
+                                                                "Undo Clicked",
+                                                                Toast.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+
+                        snackbar.show();
+                    }
+                });
+    }
+}
